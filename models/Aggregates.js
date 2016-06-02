b5a7da99a3ef3110d87b8ab4a2e60f60b1420d0f
Gman24 <stuckinabox@live.com>
2016-06-02 20:32:35 +0000

    first commit

---
+++ models/Aggregates.js
@@ -0,0 +1,19 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * AggregateList Model
+ * ==================
+ */
+var Ag = new keystone.List('Aggregate List', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true, index: true},
+	hidden: true
+});
+
+Ag.add({
+	title: { type: String, required: true },
+	items: { type: Types.TextArray }
+});
+
+Ag.register();
